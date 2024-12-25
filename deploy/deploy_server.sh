d6292e8929396066e046938c4f733f1d9691d256
hyun.kim <dss99911@gmail.com>
2024-12-25 07:58:14 +0700

    update

--- deploy/deploy_server.sh
+++ deploy/deploy_server.sh
@@ -5,4 +5,5 @@ source config.sh
 pem_path="set-your-pem-path"
 ip_address="set-your-ip-address"
 env_path="set-env-path-on-server"
+ssh -i "$pem_path" ec2-user@"$ip_address" "sudo docker rm -f $image_name" || true
 ssh -i "$pem_path" ec2-user@"$ip_address" "sudo docker run -d --restart unless-stopped -v $env_path:/opt/code/.env --name $image_name $image_uri"
