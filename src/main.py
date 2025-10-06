ac5eafc108b2ca42b867903cd91f5975aaf4d376
Hyun Kim <hyun@balancehero.com>
2025-10-07 00:03:47 +0700

    add github action

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import threading
+
+from module import chatbot, api, youtube_feed
+from utils.slack import env
+
+
+def main():
+    if env == "prod":
+        functions = [api.run, chatbot.run, youtube_feed.run]
+    else:
+        functions = [chatbot.run]
+    threads = [threading.Thread(target=f, daemon=True) for f in functions]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == '__main__':
+    main()
+    
+    
